10ffa8bd66df62b66f2e63ff296d33040cd474b1
unknown <nico.fcok@gmail.com>
2022-10-05 19:21:35 -0300

    subiendo archivos

---
+++ js/script.js
@@ -0,0 +1,102 @@
+/*
+La pagina web debe ser responsive
+Se debe poder buscar cualquier localidad (leer documentación)
+El proyecto debe estar en github
+Agregar al proyecto a los usuarios: patoogm y cajimenez96
+Cada miembro del grupo, si es los hay, debe tener al menos 1 rama en el proyecto.
+Los grupos no pueden ser mas de 3 personas*/
+
+/*
+##### Falta probar que se introduzca por nombre
+adjunto este link que seria la forma para obtener el 'name'
+https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key} 
+*/
+
+let keyAPI = '0dc14b95512a30f4dcf4cd3afc03dd24'
+
+let boton = document.getElementById('button-1')
+boton.addEventListener('click', function () {
+//  console.log(provincia)
+let codigoPostal = document.getElementById('codigo_postal').value
+let provincia = document.getElementById('provincia').value
+let codigoPais = document.getElementById('codigo').value
+
+console.log(provincia)
+console.log(codigoPostal)
+console.log(codigoPais)
+
+  if (codigoPostal == '') {
+    return alert('Faltan datos')
+  } 
+
+
+  const getCiudad = async () => {
+    const resultado = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${codigoPostal},${codigoPais}&appid=0dc14b95512a30f4dcf4cd3afc03dd24`)
+    const response = await resultado.json()
+    // console.log(response)
+    return response
+  }
+
+  const getTemperaturas = async () => {
+    const ciudad = await getCiudad();
+    const resultado = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${ciudad.lat}&lon=${ciudad.lon}&lang=es&appid=${keyAPI}`)
+    const response = await resultado.json()
+    // console.log(response)
+    return response
+  }
+
+
+  const tarjetas = async () => {
+    const ciudad = await getTemperaturas();
+    console.log(ciudad)
+    let tarjeta = document.getElementById('card')
+    tarjeta.innerHTML = `
+  <p>Temperatura actual: ${Math.ceil(ciudad.main.temp) / 10}°C</p>
+  <p>Temperatura minima: ${Math.floor(ciudad.main.temp_min / 10)}°C</p>
+  <p>Temperatura max: ${Math.floor(ciudad.main.temp_max / 10)}°C</p>
+  <p>Descripcion : ${ciudad.weather[0].description}
+  <p>Icon : <img src="http://openweathermap.org/img/wn/${ciudad.weather[0].icon}@2x.png" alt=""> </p>
+  `
+  
+
+  
+  }
+  tarjetas()
+})
+
+
+
+
+
+/*
+const getClima = async () => {
+  // const resultado = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=0dc14b95512a30f4dcf4cd3afc03dd24')
+  // const resultado = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${keyAPI}`)
+  // const resultado = await fetch('https://api.openweathermap.org/data/2.5/weather?id=3435910&appid=0dc14b95512a30f4dcf4cd3afc03dd24')
+  const resultado = await fetch('http://api.openweathermap.org/geo/1.0/zip?zip=4000,AR&appid=0dc14b95512a30f4dcf4cd3afc03dd24')
+  const response = await resultado.json()
+  
+  console.log(response)
+  
+}
+
+const pidiendoDatas = async () => {
+  const resultado = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=-26.8083&lon=-65.2333&appid=${keyAPI}`)
+  const response = await resultado.json()
+  
+  console.log(response)
+}
+
+// http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
+// https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid={API key}
+
+getClima()
+pidiendoDatas()
+*/
+
+//   const getPersonajes = async () => {
+//   const resultado = await fetch('https://rickandmortyapi.com/api/character');
+//   const personajes = await resultado.json();
+//   return personajes.results
+// }
+
