dc070ff5aeba705679f9c7e8e284525c5b467de9
unknown <nico.fcok@gmail.com>
2022-10-06 17:03:28 -0300

    modificando interfaz

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ El proyecto debe estar en github
 Agregar al proyecto a los usuarios: patoogm y cajimenez96
 Cada miembro del grupo, si es los hay, debe tener al menos 1 rama en el proyecto.
 Los grupos no pueden ser mas de 3 personas*/
+// const resultado = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${codigoPostal},${codigoPais}&appid=0dc14b95512a30f4dcf4cd3afc03dd24`)
+// const resultado = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${provincia},${codigoPais}&lang=es&appid=${keyAPI}`)
 
 /*
 ##### Falta probar que se introduzca por nombre
@@ -18,14 +20,8 @@ let keyAPI = '0dc14b95512a30f4dcf4cd3afc03dd24'
 
 const defaultApi = () => {
   const getCiudad = async () => {
-    // let codigoPostal = document.getElementById('codigo_postal').value
-    // let provincia = document.getElementById('provincia').value
-    // let codigoPais = document.getElementById('codigo').value
     let nombreCiudad = ''
-    // const resultado = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${codigoPostal},${codigoPais}&appid=0dc14b95512a30f4dcf4cd3afc03dd24`)
-    // const resultado = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${provincia},${codigoPais}&lang=es&appid=${keyAPI}`)
     const resultado = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=tucuman,ar&lang=es&appid=${keyAPI}`)
-    // const response = await resultado.json()
     const response = await resultado.json()
     nombreCiudad = response.name;
     console.log(response)
@@ -76,12 +72,12 @@ let codigoPais = document.getElementById('codigo').value
     let tarjeta = document.getElementById('card')
     tarjeta.innerHTML = `
   <img src="http://openweathermap.org/img/wn/${ciudad.weather[0].icon}@2x.png" alt="">
-  <p>${Math.ceil(ciudad.main.temp) / 10}°C</p>
-  <p class="descripcion_clima">${ciudad.weather[0].description}</p>
-  <p>${ciudad.name}</p>
+  <p class="fs-4">${Math.ceil(ciudad.main.temp) / 10}°C</p>
+  <h2 class="descripcion_clima fs-5">${ciudad.weather[0].description}</h2>
+  <h1 class="fs-3">${ciudad.name}</h1>
   <section class="d-flex justify-content-around justify-content-lg-evenly">
-  <p>Min <br> ${Math.floor(ciudad.main.temp_min / 10)}°C</p>
-  <p>Max <br> ${Math.floor(ciudad.main.temp_max / 10)}°C</p>
+  <p clas="fs-4">Min <br> ${Math.floor(ciudad.main.temp_min / 10)}°C</p>
+  <p ">Max <br> ${Math.floor(ciudad.main.temp_max / 10)}°C</p>
   </section>
   `
 }
